3e29945ff0131c58aee04dc31e87bdd569013af7
鸣涛 刘 <liumingtao412@live.com>
2018-10-18 18:00:55 +0800

    first

---
+++ WebApplication1/Controllers/LeagueController.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApplication1;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class LeagueController : ApiController
+    {
+ 
+        static League currentLeague = new League();
+
+        [HttpPost]
+        public void SetLeagueName(League l)
+        {
+            currentLeague.Name = l.Name;
+        }
+        public HttpResponseMessage GetLeague()
+        {
+            return Request.CreateResponse(HttpStatusCode.Created,currentLeague);
+        }
+        public League GetLeague2()
+        {
+            return currentLeague;
+        }
+        [HttpPost]
+        public void AddTeam(Team team)
+        {
+            //var newTeam = new Team();            
+            //newTeam.Name = team.
+            currentLeague.AddTeam(team);
+        }
+
+ 
+    }
+}
